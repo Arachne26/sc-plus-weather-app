06194564c26effeeed93faa6a7a68bc1031b62de
Arachne26 <s.wilman392@gmail.com>
2025-04-18 17:22:03 +0100

    Integrate weather API
    Get Weather API key and url
    Make search result into the input for the API key
    Display API data for city and temperature
    Set screen load city to London

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeatherInformation(response) {
+  let currentTemperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#weather-temperature");
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchWeatherCity(city) {
+  let apiKey = "9fbt478b0201aaf4ac851870f23ea7o1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeatherInformation);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+  searchWeatherCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchWeatherCity("London");
