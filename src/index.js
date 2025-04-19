8fea4e5e206c0217622b67c8f723f1b061655804
Arachne26 <s.wilman392@gmail.com>
2025-04-19 13:07:20 +0100

    Add time
    Update HTML to remove placeholder
    Create timeElement
    Get time from API
    Create formatDate function
    Display day and time

--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ function refreshWeatherInformation(response) {
   let currentConditions = response.data.condition.description;
   let currentHumidity = Math.round(response.data.temperature.humidity);
   let currentWind = Math.round(response.data.wind.speed);
+  let date = new Date(response.data.time * 1000);
 
   let temperatureElement = document.querySelector("#weather-temperature");
   let iconElement = document.querySelector("#weather-temperature-icon");
   let conditionsElement = document.querySelector("#conditions");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
 
   temperatureElement.innerHTML = currentTemperature;
   iconElement.innerHTML = icon;
   conditionsElement.innerHTML = currentConditions;
   humidityElement.innerHTML = `${currentHumidity}%`;
   windElement.innerHTML = `${currentWind} km/hr`;
+  timeElement.innerHTML = formatDate(date);
 
   let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = response.data.city;
@@ -34,6 +37,27 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   searchWeatherCity(searchInput.value);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 let searchFormElement = document.querySelector("#search-form");
